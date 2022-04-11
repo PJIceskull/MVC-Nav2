018d39b99aa75eb0168ed07d4f6786ce76fdad8b
Pierce Issah <PJIceskull2003@gmail.com>
2022-04-11 19:36:07 -0400

    added contact content to model.js

--- model/model.js
+++ model/model.js
@@ -4,7 +4,39 @@ var aboutContent = `<h1>ABOUT</h1>
 <button onclick="dynamicListener()">PRESS</button> 
 <a href="#">ABOUT Button</a>`;
 var productsContent = `<h1>PRODUCT</h1>`;
-var contactContent = `<h1>CONTACT</h1>`;
+var contactContent = `<h1>CONTACT<hr class="title-hr" />
+    </h1>
+      <form>
+        <!-- Name -->
+        <div>
+          <label>Name:</label>
+          <input class="contactBox" type="text" placeholder="Name..." name="" />
+        </div>
+        <!-- email -->
+        <div>
+          <label>Email:</label>
+          <input
+            class="contactBox"
+            type="email"
+            placeholder="Email..."
+            name=""
+          />
+        </div>
+
+        <!-- Phone number -->
+        <div>
+          <label>Phone Number:</label>
+          <input
+            class="contactBox"
+            type="text"
+            placeholder="Phone Number..."
+            name=""
+          />
+        </div>
+
+        <!-- submit box -->
+        <input class="submitBOX" type="submit" value="SUBMIT" />
+      </form>`;
 
 export var myName = "Pierce";
 
